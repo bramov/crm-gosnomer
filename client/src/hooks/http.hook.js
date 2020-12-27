7d44715c7ecc9a8cb9d4efaad55fb37cef9daad3
bramov <alexey.abramov96@gmail.com>
2020-12-27 20:10:37 +0300

    tiny bug fixes

--- client/src/hooks/http.hook.js
+++ client/src/hooks/http.hook.js
@@ -6,6 +6,10 @@ export const useHttp = () => {
   const request = useCallback(async (url, method = 'GET', body = null, headers = {}) => {
     setLoading(true);
     try {
+      if (body) {
+        body = JSON.stringify(body);
+        headers['Content-Type'] = 'application/json';
+      }
       const response = await fetch(url, {method, body, headers});
       const data = await response.json();
 
@@ -17,7 +21,7 @@ export const useHttp = () => {
     } catch (e) {
       setLoading(false);
       setError(e.message);
-      throw e
+      throw e;
     }
   }, [])
 
--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,20 +1,28 @@
-import React from 'react';
-
+import React, {useState} from 'react';
+import {useHttp} from "../hooks/http.hook";
+import logo from '../images/gosnomer.png';
 export const AuthPage = () => {
-
+  const { loading, request } = useHttp()
   const [form, setForm] = useState({
     email: '',
     password: ''
-  })
+  });
 
   const changeHandler = (event) => {
     setForm({ ...form, [event.target.name]: event.target.value })
   }
 
+  const registerHandler = async () => {
+    try {
+      const data = await request('/api/auth/register', 'POST', {...form});
+      console.log(data);
+    } catch (e) {}
+  }
+
   return (
     <div className="row">
       <div className="col s6 offset-s3">
-        <div className="center-align"><img src="../../public/gosnomer.png"/></div>
+        <div className="center-align"><img src={logo} width="150"/></div>
         <div className="card darken-1">
           <div className="card-content gray-text">
             <span className="card-title center-align">Авторизация</span>
@@ -44,10 +52,15 @@ export const AuthPage = () => {
               </div>
 
             </div>
-          </div>
-          <div className="card-action row s-6">
-            <button className="col s-6 btn yellow darken-4">Войти</button>
-            <button className="btn grey lighten-1 black-text col offset-s6">Регистрация</button>
+            <div className="card-action row s-6">
+              <button className="col s-6 btn yellow darken-4"
+                      disabled={loading}
+              >Войти</button>
+              <button className="btn grey lighten-1 black-text col offset-s6"
+                      onClick={registerHandler}
+                      disabled={loading}
+              >Регистрация</button>
+            </div>
           </div>
         </div>
       </div>
